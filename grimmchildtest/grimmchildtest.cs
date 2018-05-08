80cf94a15d891a2a611b65817f785318e624fbb6
Eli Stone <eli.stonium@gmail.com>
2018-05-08 01:18:07 -0700

    Add simple test for grimmchild checker modifier, to find bugs in wrong results

---
+++ grimmchildtest/grimmchildtest.cs
@@ -0,0 +1,34 @@
+﻿using Modding;
+
+namespace grimmchildtest
+{
+    public class GCtest : Mod
+    {
+        public override string GetVersion()
+        {
+            return "1";
+        }
+
+        public override void Initialize()
+        {
+            ModHooks.Instance.GetPlayerBoolHook += fakeNodefeatGrimm;
+        }
+
+        public bool fakeNodefeatGrimm(string originalSet)
+        {
+            if (originalSet == "defeatedNightmareGrimm")
+            {
+                return false;
+            }
+            else if (originalSet == "troupeInTown")
+            {
+                return true;
+            }
+            else if (originalSet == "equippedCharm_40")
+            {
+                return true;
+            }
+            return PlayerData.instance.GetBoolInternal(originalSet);
+        }
+    }
+}
