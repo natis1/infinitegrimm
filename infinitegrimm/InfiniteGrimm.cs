f34cc67b98202a36c0f4dce47eb5262c754ea8ba
Eli Stone <eli.stonium@gmail.com>
2018-05-07 20:21:28 -0700

    Closes #4 by properly commenting the actual grimm fight where all the real code is

--- infinitegrimm/InfiniteGrimm.cs
+++ infinitegrimm/InfiniteGrimm.cs
@@ -22,7 +22,6 @@ namespace infinitegrimm
         bool runningIG;
 
         int damageDone;
-        int lastDifficultyIncrease;
         int lastBalloonDamage;
         // Health just needs to be high enough that grimm doesn't use the balloon attack (and can't be killed) naturally
         int defaultHealth;
@@ -50,34 +49,34 @@ namespace infinitegrimm
 
         public void Reset(Scene from, Scene to)
         {
-            Modding.Logger.Log("[Infinite Grimm] Scene name: " + to.name + " defeat NKG: " + PlayerData.instance.GetBoolInternal("defeatedNightmareGrimm") + " kill nkg? " + PlayerData.instance.killedNightmareGrimm);
+            // Essentially makes sure they complete grimm quest before starting infinite grimm.
+            // Otherwise the player would have a hard time upgrading their grimmchild.
             if (to.name == "Grimm_Nightmare")
             {
                 if (PlayerData.instance.GetBoolInternal("defeatedNightmareGrimm") && PlayerData.instance.killedNightmareGrimm)
                 {
-                    attacksToStun = 8;
-                    danceSpeed = 0.8;
                     damageDone = 0;
                     lastBalloonDamage = 0;
-                    lastDifficultyIncrease = 0;
                     stunCounter = 0;
+                    // Just needs to be high enough he won't naturally die or use balloon attack.
                     defaultHealth = 3000;
-                    grimm = GameObject.Find("Grimm Control");
-                    grimm_anim_obj = grimm.FindGameObjectInChildren("Nightmare Grimm Boss");
-                    grimm_anim = grimm_anim_obj.GetComponent<tk2dSpriteAnimator>();
 
+                    // Assigning the FSMs.
+                    grimm_anim_obj = GameObject.Find("Grimm Control").FindGameObjectInChildren("Nightmare Grimm Boss");
+                    grimm_anim = grimm_anim_obj.GetComponent<tk2dSpriteAnimator>();
                     controlFSM = FSMUtility.LocateFSM(grimm, "Control");
                     controlAnimFSM = FSMUtility.LocateFSM(grimm_anim_obj, "Control");
                     stunFSM = FSMUtility.LocateFSM(grimm_anim_obj, "Stun");
 
-
-                    FsmState outpause = controlAnimFSM.GetState("Out Pause");
-                    outpause.ClearTransitions();
-                    outpause.AddTransition("FINISHED", "Move Choice");
-                    runningIG = true;
                     hm = grimm_anim_obj.GetComponent<HealthManager>();
                     hm.hp = defaultHealth;
 
+                    // Starts the update process. This just saves CPU cycles when not in the grimm room.
+                    // It is done after loading the FSMs in case they error out and the Reset function never finishes.
+                    runningIG = true;
+
+                    // We implement our own balloon control. So we need to track which ones have been used.
+                    // Balloon 2 makes the scene look more interesting, and happens after 840 damage in our setup.
                     balloon1 = false;
                     balloon2 = false;
 
@@ -87,23 +86,23 @@ namespace infinitegrimm
                     normalStun.RemoveActionsOfType<IntAdd>();
                     
                     // Stops FSM from doing stuns because we are implementing them in code.
+                    // This essentially makes the stun FSM stuck in the "In Combo" state
+                    // Until we manually tell it to be in the "Stun" state.
                     FsmState stunCombo = stunFSM.GetState("In Combo");
                     stunCombo.ClearTransitions();
 
-
+                    // These variables are used in the difficulty section
+                    // Be sure to set attacks to stun and dance speed both here and there.
+                    attacksToStun = 8;
+                    danceSpeed = 0.8;
                     teleinFPS = grimm_anim.GetClipByName("Tele In").fps;
                     teleoutFPS = grimm_anim.GetClipByName("Tele Out").fps;
                     uppercutendFPS = grimm_anim.GetClipByName("Uppercut End").fps;
                     slashrecoverFPS = grimm_anim.GetClipByName("Slash Recover").fps;
                     evadeendFPS = grimm_anim.GetClipByName("Evade End").fps;
 
-
-
                     // Actually we're just setting the difficulty the first time this is run.
                     increaseDifficulty();
-
-
-
 
                     Modding.Logger.Log("[Infinite Grimm] Setup Nightmare IG battle");
                 }
@@ -113,16 +112,18 @@ namespace infinitegrimm
             }
         }
 
-        // place code to be run every frame of the infinite grimm.
-        
+        // Code to be run every frame of the infinite grimm fight, including before he appears
         public void Update()
         {
             if (runningIG)
             {
+
+                // This is the bulk of the running code. Tracks grimm damage taken to make him stronger.
                 takeDamage();
                 if (PlayerData.instance.health <= 0)
                 {
                     // The death trigger doesn't affect dream bosses?
+                    // So instead we need to trigger it manually with this check
                     playerDies();
                 }
 
@@ -135,6 +136,8 @@ namespace infinitegrimm
         {
             if (hm.hp != defaultHealth)
             {
+                // Tracks the damage done without actually hurting grimm.
+                // Also applies stun if needed
                 damageDone = damageDone + (defaultHealth - hm.hp);
                 hm.hp = defaultHealth;
                 stunCounter++;
@@ -144,14 +147,9 @@ namespace infinitegrimm
                     stunCounter = 0;
 
                 }
-            }
-
-            // don't waste cpu recalculating difficulty every frame. only on hits with real weapons
-            if (damageDone - lastDifficultyIncrease > 10)
-            {
-                lastDifficultyIncrease = damageDone;
+                // don't waste cpu recalculating difficulty every frame. only after some damage.
                 increaseDifficulty();
-            }
+            }            
 
             // balloons should happen at increasingly rare intervals.
             // after: 400, 440, 484, 532, 585... etc damage. This lets the player keep getting stuns in even as stun rarity goes up
@@ -203,12 +201,15 @@ namespace infinitegrimm
             }
         }
 
-        // place code to give player geo on death based on damage done. Or whatever else you want to do
+        // Gives geo on death and turns off the script.
+        // Eventually I want a better setup for this.
         public void playerDies()
         {
             Modding.Logger.Log("[Infinite Grimm] Good job, you did: " + damageDone + " damage!");
             int geo = (int) (damageDone / 10.0);
             PlayerData.instance.AddGeo(geo);
+
+            // This saves cpu cycles and potentially stops the player from getting Geo multiple times.
             runningIG = false;
         }
     }
