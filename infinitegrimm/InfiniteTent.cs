d1fb653e32378b6209ed78b7a48cb17190fb04fa
Eli Stone <eli.stonium@gmail.com>
2018-05-12 00:31:57 -0700

    Closes #25 I think. hard to tell, might need to reopen

--- infinitegrimm/InfiniteTent.cs
+++ infinitegrimm/InfiniteTent.cs
@@ -31,6 +31,9 @@ namespace infinitegrimm
 
         public GameObject grimm;
 
+        public PlayMakerFSM interactions;
+        public bool didReturn;
+
         // This code inspired by Randomizer Mod 2.0
         private static Dictionary<string, Dictionary<string, string>> langStrings;
 
@@ -71,6 +74,75 @@ namespace infinitegrimm
 
         }
 
+        private void setupGrimm()
+        {
+            grimm = GameObject.Find("Grimm Holder");
+            grimm.SetActive(true);
+            FsmState f = FSMUtility.LocateFSM(grimm, "Chest Control").GetState("Init");
+            f.ClearTransitions();
+            // hack it so grimm always appears... dead and alive.
+            f.AddTransition("FINISHED", "Killed");
+
+            FsmState dead = FSMUtility.LocateFSM(grimm, "Chest Control").GetState("Killed");
+            dead.AddTransition("FINISHED", "Appear");
+            GameObject grimmScene = grimm.FindGameObjectInChildren("Grimm Scene");
+            interactions = FSMUtility.LocateFSM(grimmScene, "Initial Scene");
+
+            FsmState initAppear = interactions.GetState("Init");
+            initAppear.ClearTransitions();
+            initAppear.AddTransition("FINISHED", "Meet Ready");
+            if (hardmode)
+            {
+                FsmState meeting = interactions.GetState("Meet Ready");
+                meeting.ClearTransitions();
+                meeting.AddTransition("ENTER", "Grimm Appear");
+
+                FsmState appear = interactions.GetState("Grimm Appear");
+                appear.RemoveActionsOfType<SendEventByName>();
+                appear.ClearTransitions();
+                appear.AddTransition("FINISHED", "Tele Out Anim");
+                Wait[] skipwait = appear.GetActionsOfType<Wait>();
+                for (int i = 0; i < skipwait.Length; i++)
+                {
+                    skipwait[i].time = (float)0.5;
+                }
+            }
+            else
+            {
+
+                // skip the long cutscene
+                FsmState fastEnter = interactions.GetState("Take Control");
+
+                fastEnter.ClearTransitions();
+
+                fastEnter.AddTransition("LAND", "Grimm Appear");
+
+                FsmState fastEnter2 = interactions.GetState("Grimm Appear");
+                fastEnter2.RemoveActionsOfType<Wait>();
+                SendEventByName[] skipwait = fastEnter2.GetActionsOfType<SendEventByName>();
+                for (int i = 0; i < skipwait.Length; i++)
+                {
+                    if (skipwait[i].delay.Value > 0.1)
+                    {
+                        skipwait[i].delay.Value = (float)0.3;
+                    }
+                }
+                FsmState greet = interactions.GetState("Meet 1");
+                greet.ClearTransitions();
+                greet.AddTransition("CONVO_FINISH", "Box Down 3");
+
+                FsmState boxDown = interactions.GetState("Box Down 3");
+
+                boxDown.ClearTransitions();
+                boxDown.AddTransition("FINISHED", "Tele Out Anim");
+
+
+            }
+            FsmState endState = interactions.GetState("End");
+            endState.AddTransition("FINISHED", "Check");
+
+        }
+
         private void loadGrimm(Scene from, Scene to)
         {
             // Yikes, talk about hacky. Reload the scene since this hook applies after the scene has already loaded in a bad way.
@@ -87,11 +159,9 @@ namespace infinitegrimm
                 enterTent = false;
             } else if (to.name == "Grimm_Main_Tent" && trueFromName == "Town")
             {
-                Modding.Logger.Log("[Infinite Grimm] Loading Grimm in tent");
-                grimm = GameObject.Find("Grimm Holder");
                 if (PlayerData.instance.killedNightmareGrimm)
                 {
-                    grimm.SetActive(true);
+                    didReturn = false;
 
                     // Setup conversation text.
                     System.Random rnd = new System.Random();
@@ -120,145 +190,21 @@ namespace infinitegrimm
                             break;
                     }
 
+                    if (!PlayerData.instance.GetBoolInternal("equippedCharm_40"))
+                        deletGrimmChild = true;
 
-                    FsmState f = FSMUtility.LocateFSM(grimm, "Chest Control").GetState("Init");
-                    f.ClearTransitions();
-                    // hack it so grimm always appears... dead and alive.
-                    f.AddTransition("FINISHED", "Killed");
+                    updatewait = 40;
 
-                    FsmState dead = FSMUtility.LocateFSM(grimm, "Chest Control").GetState("Killed");
-                    dead.AddTransition("FINISHED", "Appear");
-
-                    //then select the child to change dialog
-
-
-                    // EN-CP2 for the second "dlc"
-                    /* all events for grimm in order for the language files but not appearance:
-                     * GRIMM_DEFEAT_2: Look here...
-                     * GRIMM_RITUAL_COMPLETE: Across these lands...
-                     * QUEEN_GRIMMCHILD_FULL: Your companion's eyes burn...
-                     * GRIMM_DREAM: Masterful! Even a vessel...
-                     * GRIMM_DEFEAT_1: Bravo, my friend. Hear...
-                     * GRIMM_DEFEAT_3: Go out into the darkness..
-                     * GRIMM_BATTLE_1: Wonderful. Wonderful! My kin...
-                     * GRIMM_BATTLE_2: This searing fire... It carries...
-                     * GRIMM_BATTLE_3: Dance with me, my friend...
-                     * GRIMM_ACCEPT: As the lantern flared your...
-                     * GRIMM_LEVELUP_1: I can feel it. The warmth..
-                     * GRIMM_LEVELUP_2: A masterful opening act...
-                     * GRIMM_LEVELUP_3: Beautiful, yes. The child has...
-                     * GRIMM_MEET1: So, it was you who called us.
-                     * GRIMM_MEET2: Well met, my friend. Well met...
-                     * 
-                     */
-
-                    GameObject grimmScene = grimm.FindGameObjectInChildren("Grimm Scene");
-                    PlayMakerFSM interactions = FSMUtility.LocateFSM(grimmScene, "Initial Scene");
-
-                    FsmState initAppear = interactions.GetState("Init");
-                    initAppear.ClearTransitions();
-                    initAppear.AddTransition("FINISHED", "Meet Ready");
-
-
-                    // skip the pausing part on hardmode to save time
-                    if (hardmode)
-                    {
-                        FsmState meeting = interactions.GetState("Meet Ready");
-                        meeting.ClearTransitions();
-                        meeting.AddTransition("ENTER", "Grimm Appear");
-
-                        FsmState appear = interactions.GetState("Grimm Appear");
-                        appear.RemoveActionsOfType<SendEventByName>();
-                        appear.ClearTransitions();
-                        appear.AddTransition("FINISHED", "Tele Out Anim");
-                        Wait[] skipwait = appear.GetActionsOfType<Wait>();
-                        for (int i = 0; i < skipwait.Length; i++)
-                        {
-                            skipwait[i].time = (float)0.5;
-                        }
-
-                        if (!PlayerData.instance.GetBoolInternal("equippedCharm_40"))
-                            deletGrimmChild = true;
-                        updatewait = 50;
-                    }
-                    else
-                    {
-
-                        // skip the long cutscene
-                        FsmState fastEnter = interactions.GetState("Take Control");
-
-                        fastEnter.ClearTransitions();
-
-                        fastEnter.AddTransition("LAND", "Grimm Appear");
-
-                        FsmState fastEnter2 = interactions.GetState("Grimm Appear");
-                        fastEnter2.RemoveActionsOfType<Wait>();
-                        SendEventByName[] skipwait = fastEnter2.GetActionsOfType<SendEventByName>();
-                        for (int i = 0; i < skipwait.Length; i++)
-                        {
-                            if (skipwait[i].delay.Value > 0.1)
-                            {
-                                skipwait[i].delay.Value = (float)0.3;
-                            }
-                        }
-                        FsmState greet = interactions.GetState("Meet 1");
-                        greet.ClearTransitions();
-                        greet.AddTransition("CONVO_FINISH", "Box Down 3");
-
-                        FsmState boxDown = interactions.GetState("Box Down 3");
-
-                        boxDown.ClearTransitions();
-                        boxDown.AddTransition("FINISHED", "Tele Out Anim");
-
-                        if (!PlayerData.instance.GetBoolInternal("equippedCharm_40"))
-                            deletGrimmChild = true;
-                        updatewait = 50;
-                    }
-                    FsmState endState = interactions.GetState("End");
-                    endState.AddTransition("FINISHED", "Check");
-
+                    setupGrimm();
                     Modding.Logger.Log("[Infinite Grimm] Loaded Grimm without error");
+
 
                     //todo remove unneeded animations here
 
                 }
             } else if (to.name == "Grimm_Main_Tent" && trueFromName == "Grimm_Nightmare" && damageDone != -1)
             {
-                Modding.Logger.Log("[Infinite Grimm] Loading tent from nightmare.");
-                grimm = GameObject.Find("Grimm Holder");
-                grimm.SetActive(true);
-
-                FsmState f = FSMUtility.LocateFSM(grimm, "Chest Control").GetState("Init");
-                f.ClearTransitions();
-                // hack it so grimm always appears... dead and alive.
-                f.AddTransition("FINISHED", "Killed");
-
-
-                FsmState dead = FSMUtility.LocateFSM(grimm, "Chest Control").GetState("Killed");
-                dead.AddTransition("FINISHED", "Appear");
-
-                GameObject grimmScene = grimm.FindGameObjectInChildren("Grimm Scene");
-                PlayMakerFSM interactions = FSMUtility.LocateFSM(grimmScene, "Initial Scene");
-
-                FsmState initAppear = interactions.GetState("Init");
-                initAppear.ClearTransitions();
-                Wait addDelay = new Wait();
-
-                // I'd love to make this 0, but it needs to be longer than however long it takes
-                // for the scene to load or you get a white screen, so I have to set it to a number high enough
-                // that this works on crappier computers. This is a dumb hack.
-                addDelay.time = 4f;
-                initAppear.AddAction(addDelay);
-                initAppear.AddTransition("FINISHED", "Meet 1");
-                
-
-                FsmState greet = interactions.GetState("Meet 1");
-                greet.ClearTransitions();
-                greet.AddTransition("CONVO_FINISH", "Box Down 3");
-
-                FsmState boxDown = interactions.GetState("Box Down 3");
-                boxDown.ClearTransitions();
-                boxDown.AddTransition("FINISHED", "End");
+                didReturn = true;
 
                 langStrings["GRIMM_MEET1"] = new Dictionary<string, string>();
                 if (damageDone == 0)
@@ -287,11 +233,15 @@ namespace infinitegrimm
                 {
                     langStrings["GRIMM_MEET1"].Add("CP2", "\n\nUnbelievable, you did " + damageDone + " damage on Hard Mode! You are no mere vessel, but a god, and nothing can stand in your way!");
                 }
-                interactions.SetState("Init");
+                
 
                 if (!PlayerData.instance.GetBoolInternal("equippedCharm_40"))
                     deletGrimmChild = true;
-                updatewait = 50;
+                updatewait = 100;
+
+                setupGrimm();
+
+
 
                 Modding.Logger.Log("[Infinite Grimm] Finished loading tent from nightmare.");
             }
@@ -333,6 +283,11 @@ namespace infinitegrimm
                     } else if (!deletGrimmChild)
                     {
                         grimmchild = grimmChild;
+                    }
+
+                    if (didReturn)
+                    {
+                        interactions.SetState("Meet 1");
                     }
                     deletGrimmChild = false;
                 }
