a941983fe5e68feac05aef218c1e95dbd60d66fd
Eli Stone <eli.stonium@gmail.com>
2018-06-11 01:13:26 -0700

    Bump version for memes

--- infinitegrimm/infinite_global_vars.cs
+++ infinitegrimm/infinite_global_vars.cs
@@ -4,7 +4,7 @@ namespace infinitegrimm
 {
     public static class infinite_global_vars
     {
-        public const string VERSION = "0.3.5";
+        public const string VERSION = "0.3.6";
         public const int LOAD_ORDER = 25;
         public static int maximumDamage;
         
